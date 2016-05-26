84739a0e00fff656efefdcc26f10b03266b220b9
moetime <morrice_pfeiffer@hotmail.com>
2016-05-26 19:37:09 -0400

    Add method for active highlighting on navbar

--- COMP2007-S2016-LESSON2/Site1.Master.cs
+++ COMP2007-S2016-LESSON2/Site1.Master.cs
@@ -11,7 +11,38 @@ namespace COMP2007_S2016_LESSON2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            addActiveClass();
+        }
 
+
+        /** This method adds active highlighter to each li in the main nav bar
+         * @ method addactiveclass
+         * @ return (string)
+         *   
+         */
+
+        private string addActiveClass()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Products":
+                    products.Attributes.Add("class", "active");
+                    break;
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+                case "About":
+                    about.Attributes.Add("class", "active");
+                    break;
+            }
+
+            return addActiveClass();
         }
     }
 }
